1592c8d67d6053fa51f7e5e88ab713b9a204d3e1
Jxck <block.rxckin.beats@gmail.com>
2012-10-06 23:55:18 +0900

    add test

--- test/test.simple.js
+++ test/test.simple.js
@@ -3,7 +3,6 @@ var assert = require('assert')
   , async = require('async')
   , helper = require('./helper');
 
-
 var DB = require('../');
 
 var fixture = {
@@ -12,29 +11,45 @@ var fixture = {
   , dbfile: __dirname + '/../db/test.db'
 };
 
-helper.reset(fixture.dbfile);
-var db = new DB(fixture.dbfile, function() {
-  var count = 0
-    , key = fixture.key
-, val = fixture.value;
+(function() {
+  // truncate the test db file
+  helper.reset(fixture.dbfile);
 
-
-  async.whilst(
-    function() {
-      count++;
-      key += 'a';
-      return count < 6;
-    },
-    function(callback) {
-      log(key, val);
-      db.set(key, val, function() {
-        db.get(key, function(value) {
-          assert.equal(val, value);
-          callback();
-        });
+  var db = new DB(fixture.dbfile, function() {
+    db.set(fixture.key, fixture.value, function() {
+      db.get(fixture.key, function(value) {
+        assert.equal(fixture.value, value);
       });
-    },
-    console.error
-  );
-});
+    });
+  });
+})();
 
+(function() {
+  // truncate the test db file
+  helper.reset(fixture.dbfile);
+
+  var db = new DB(fixture.dbfile, function() {
+    var count = 0
+      , key = fixture.key
+      , val = fixture.value
+      ;
+
+    async.whilst(
+      function() {
+        count++;
+        key += 'a';
+        return count < 6;
+      },
+      function(callback) {
+        log(key, val);
+        db.set(key, val, function() {
+          db.get(key, function(value) {
+            assert.equal(val, value);
+            callback();
+          });
+        });
+      },
+      console.error
+    );
+  });
+})();
