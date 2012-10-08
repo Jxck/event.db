15f950a8ab866fe3925f0ac97d60fa54c81f80ba
Jxck <block.rxckin.beats@gmail.com>
2012-10-08 18:35:47 +0900

    add config loader

---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = (function() {
+  var command = process.argv.indexOf('--config');
+  var configfile;
+  if(command > 0) {
+    configfile = process.argv[command + 1]
+  } else {
+    configfile = __dirname + '/default.json';
+  }
+  return require(configfile);
+})();
