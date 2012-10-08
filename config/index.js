a45c9249e9df51ae91a6d767ac2456e347f4eaf5
Jxck <block.rxckin.beats@gmail.com>
2012-10-08 18:57:41 +0900

    make row.js to load config

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = (function() {
   var command = process.argv.indexOf('--config');
   var configfile;
-  if(command > 0) {
-    configfile = process.argv[command + 1]
+  if (command > 0) {
+    configfile = process.argv[command + 1];
   } else {
     configfile = __dirname + '/default.json';
   }
