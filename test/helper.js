439279a91d76f9d63dff9098215f6ed923fbb11b
Jxck <block.rxckin.beats@gmail.com>
2012-11-03 17:10:44 +0900

    cleanup

--- test/helper.js
+++ test/helper.js
@@ -14,21 +14,3 @@ module.exports.filename = function(name) {
 module.exports.log = function(name, testcount) {
   console.error('\033[35m' + module.exports.filename(name) + ': ' + testcount + '\033[0m');
 };
-
-module.exports.test = function() {
-  return function tests(fn) {
-    if (!tests.funcs) tests.funcs = [];
-    if (fn) {
-      tests.funcs.push(fn);
-      return tests;
-    }
-    function next() {
-      if (tests.funcs.length) {
-        var fn = tests.funcs.shift();
-        fn(next);
-      }
-    }
-    var first = tests.funcs.shift();
-    return first(next);
-  };
-};
--- test/index.js
+++ test/index.js
@@ -2,5 +2,3 @@ require('./test.row');
 require('./test.simple');
 require('./test.utils');
 require('./test.sockets');
-require('./test.helper');
-require('./test.helper2');
